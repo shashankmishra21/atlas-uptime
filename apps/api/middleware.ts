a118e4122eb2aae4ebedefd6aee299c4be8a8d18
Shashank Mishra <mishrashashank2106@gmail.com>
2026-06-22 14:42:37 +0530

    fix: middleware setup using clerk

--- apps/api/middleware.ts
+++ apps/api/middleware.ts
@@ -1,8 +1,20 @@
-import type { Request, Response, NextFunction } from "express";
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_PUBLIC_KEY } from "./config";
 
-export function authMiddleware(req: Request, res:Response, next:NextFunction){
-    const authHeader  = req.headers['authorization'];
+export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+    const token = req.headers['authorization'];
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
 
-    req.userId = "1";
-    next();
+    const decoded = jwt.verify(token, JWT_PUBLIC_KEY);
+    console.log(decoded);
+    if (!decoded || !decoded.sub) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    req.userId = decoded.sub as string;
+    
+    next()
 }
